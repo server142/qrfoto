bb29c9efd46edb7c7cf15e4ae62e367e61b5635f
server 142 <server142@hotmail.com>
2026-04-04 02:38:03 -0600

    feat: seed super admin account automatically on boot

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
+import { User, UserRole } from './entities/user.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import * as bcrypt from 'bcrypt';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
@@ -10,4 +13,26 @@ import { UsersController } from './users.controller';
   providers: [UsersService],
   exports: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule implements OnModuleInit {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) { }
+
+  async onModuleInit() {
+    const adminEmail = 'admin@qrfoto.com';
+    const existingAdmin = await this.userRepository.findOne({ where: { email: adminEmail } });
+    if (!existingAdmin) {
+      const hashedPassword = await bcrypt.hash('AdminQRFoto2026!', 10);
+      const admin = this.userRepository.create({
+        first_name: 'Super',
+        last_name: 'Admin',
+        email: adminEmail,
+        password_hash: hashedPassword,
+        role: UserRole.SUPER_ADMIN,
+      });
+      await this.userRepository.save(admin);
+      console.log('Seed: SuperAdmin account created.');
+    }
+  }
+}
